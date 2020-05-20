de2437f2512b1f3ba6c9c7be217c861fbd86261a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-20 23:30:25 +0000

    Deploy RomanovAleksander/chuck-norris-jokes to github.com/RomanovAleksander/chuck-norris-jokes.git:gh-pages

---
+++ precache-manifest.990ca7a1460979c7d6c15cf14cc3e2b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a408cfbf08167ba99f0413d1747cbd14",
+    "url": "/chuck-norris-jokes/index.html"
+  },
+  {
+    "revision": "9ffef199594287c85bbc",
+    "url": "/chuck-norris-jokes/static/css/main.cd221e92.chunk.css"
+  },
+  {
+    "revision": "0db7095bd5abab5c0b37",
+    "url": "/chuck-norris-jokes/static/js/2.018d98a4.chunk.js"
+  },
+  {
+    "revision": "9ffef199594287c85bbc",
+    "url": "/chuck-norris-jokes/static/js/main.d2870cc6.chunk.js"
+  },
+  {
+    "revision": "028f5936122dfbaf60d9",
+    "url": "/chuck-norris-jokes/static/js/runtime-main.42ddd337.js"
+  },
+  {
+    "revision": "2e60bb135a597a545868db379f2a66d6",
+    "url": "/chuck-norris-jokes/static/media/arrow.2e60bb13.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chuck-norris-jokes/precache-manifest.f6ccc5056bbbc90126e75fdb35172d6a.js"
+  "/chuck-norris-jokes/precache-manifest.990ca7a1460979c7d6c15cf14cc3e2b7.js"
 );
 
 self.addEventListener('message', (event) => {
